1640e1f96a4c759565fade71b618c64e299017b0
fummicc1 <fumiya.tennis1@gmail.com>
2021-11-14 01:29:59 +0900

    Done

--- SwiftZoomin8/UserViewState.swift
+++ SwiftZoomin8/UserViewState.swift
@@ -3,7 +3,8 @@ import Foundation
 
 import class UIKit.UIImage
 
-final class UserViewState {
+@MainActor
+final class UserViewState: ObservableObject {
     let id: User.ID
     
     @Published private(set) var user: User?
@@ -11,5 +12,32 @@ final class UserViewState {
     
     init(id: User.ID) {
         self.id = id
+    }
+
+    func loadUser() async {
+        do {
+            // User の JSON の取得
+            let url: URL = .init(string: "https://koherent.org/fake-service/api/user?id=\(id)")!
+            let data = try await downloadData(from: url)
+
+            // JSON のデコード
+            let user: User = try JSONDecoder().decode(User.self, from: data)
+
+            self.user = user
+
+            // アイコン画像の取得
+            let iconData = try await downloadData(from: user.iconURL)
+            // Data を UIImage に変換
+            guard let iconImage: UIImage = .init(data: iconData) else {
+                // エラーハンドリング
+                print("The icon image at \(user.iconURL) has an illegal format.")
+                return
+            }
+            self.iconImage = iconImage
+
+        } catch {
+            // エラーハンドリング
+            print(error)
+        }
     }
 }
